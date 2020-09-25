44f7c9017d2e6132739d9491acc5c826f93bba45
Daniel Presas <daniel.presas@ontariotechu.net>
2020-09-25 00:32:34 -0400

    DLL stuff is working...Cleanup test stuff later

---
+++ Assets/Scripts/DLLTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TestDLL;
+
+public class DLLTest : MonoBehaviour
+{
+    void Start()
+    {
+        DLLClass test = new DLLClass();
+        int rand = test.GetRandomInt();
+        print("Random int is: " + rand);
+    }
+}
