612108ce2d2d1c04d3e0f48229342fdf84df4f42
Anthony <anth19800@hotmail.com>
2020-10-06 14:01:10 -0400

    [Wip] Safety Commit

---
+++ Assets/Scripts/NPlayerInput.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NPlayerInput : MonoBehaviour
+{
+
+    Vector2 _moveInput;
+    Vector2 _mouseInput;
+    public float moveSpeed = 5.0f;
+
+    //Camera
+    public GameObject pCamera;
+    public GameObject player;
+    public GameObject lookingAt;
+    public float yUpperBound = 4.0f;
+    public float yLowerBound = -3.0f;
+    public float rotationSpeedInverse = 1.0f;
+    //End of Camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _MouseInput();
+        _Move();
+    }
+
+    void _MouseInput()
+    {
+        if (rotationSpeedInverse < 1.0f)
+            rotationSpeedInverse = 1.0f;
+
+        //rotate camera on x z plane
+        player.transform.rotation = Quaternion.AngleAxis(_mouseInput.x / rotationSpeedInverse, Vector3.up) * player.transform.rotation;
+
+        //increase/decrease height of camera target
+        lookingAt.transform.position = lookingAt.transform.position + new Vector3(0.0f, _mouseInput.y / (rotationSpeedInverse * 10.0f), 0.0f);
+
+
+        //clamp y position
+        if (lookingAt.transform.localPosition.y > yUpperBound)
+            lookingAt.transform.localPosition = new Vector3(lookingAt.transform.localPosition.x, yUpperBound, lookingAt.transform.localPosition.z);
+        else if (lookingAt.transform.localPosition.y < yLowerBound)
+            lookingAt.transform.localPosition = new Vector3(lookingAt.transform.localPosition.x, yLowerBound, lookingAt.transform.localPosition.z);
+
+        //lock on to target
+        pCamera.transform.LookAt(lookingAt.transform);
+    }
+
+    void _Move()
+    {
+        float y = gameObject.GetComponent<Rigidbody>().velocity.y;
+        Vector3 vel = Quaternion.AngleAxis(270, Vector3.up) * ((Quaternion.AngleAxis(180, Vector3.up) * (transform.forward * _moveInput.x)) + (Quaternion.AngleAxis(90, Vector3.up) * (transform.forward * _moveInput.y))) * moveSpeed;
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(vel.x, y, vel.z);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnMouseInput(InputAction.CallbackContext ctx)
+    {
+        _mouseInput = ctx.ReadValue<Vector2>();
+    }
+
+}
--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Options : MonoBehaviour
 {
@@ -8,8 +9,6 @@ public class Options : MonoBehaviour
     //@Temporary: only for debugging. Will delete later
     public bool lockCurser = true;
 
-    public PlayerManager playerManager;
-    public ViewportManager viewportManager;
     public int numPlayers = 1;
 
     bool enableCouchCoop = true;
@@ -17,19 +16,6 @@ public class Options : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        setupPlayersAndViewports();
-    }
-
-    void setupPlayersAndViewports()
-    {
-        playerManager.ActivePlayerSetup(numPlayers);
-
-        List<Camera> tempCameras = new List<Camera>();
-
-        for (int i = 0; i < 4; i++)
-            tempCameras.Add(playerManager.players[i].GetComponentInChildren<Camera>());
-
-        viewportManager.SpaceViewportsAppropriately(numPlayers, tempCameras);
     }
 
     int _lastNumPlayers = 0;
@@ -41,9 +27,5 @@ public class Options : MonoBehaviour
         else
             Cursor.lockState = CursorLockMode.None;
 
-        //optimization
-        if (numPlayers != _lastNumPlayers)
-            setupPlayersAndViewports();
-        _lastNumPlayers = numPlayers;
     }
 }
