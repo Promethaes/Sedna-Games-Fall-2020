b2111aada165d54988f7816d8e23ebbc551af410
Anthony <anth19800@hotmail.com>
2020-10-24 21:13:38 -0400

    [Add] Enemy Spawner

---
+++ Assets/Scripts/Enemy Scripts/Enemy Spawner Scripts/EnemySpawnPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    public float spawnRadiusScalar = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>().spawnPoints.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawnEnemy(GameObject enemy)
+    {
+        float radius = gameObject.GetComponent<SphereCollider>().radius;
+
+        enemy.transform.position = gameObject.transform.position + new Vector3(Random.Range(-radius,radius),0.0f,Random.Range(-radius,radius))*spawnRadiusScalar;
+        enemy.SetActive(true);
+    }
+
+}
