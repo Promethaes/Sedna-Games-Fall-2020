ecf1a021d2b60790ebd90383ea10b3464984b1f5
Daniel Presas <daniel.presas@ontariotechu.net>
2020-10-26 14:52:25 -0400

    [Add] Player health UI, only reports self

---
+++ Assets/Scripts/UI/PlayerHealthUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class PlayerHealthUI : MonoBehaviour
+{
+    [SerializeField] private PlayerBackend _backend = null;
+    public float maximum = 100.0f;
+
+    [Range(0.0f, 100.0f)]
+    public float currentFill = 100.0f;
+
+    public Image fillMask;
+
+    void Start()
+    {
+        if(_backend == null) Debug.LogAssertion("No backend for player health UI!");
+        currentFill = maximum = _backend.hp;
+    }
+
+    void Update()
+    {
+        currentFill = _backend.hp;
+        fillMask.fillAmount = currentFill / maximum;
+    }
+
+
+}
