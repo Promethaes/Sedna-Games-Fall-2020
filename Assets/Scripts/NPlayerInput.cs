cc95246dfe800c615ceebd0694f1a16d77c45a7d
Synral <44206227+Synral@users.noreply.github.com>
2020-11-03 12:50:42 -0500

    [Update] Movement System & Camera System
    + Proper _isGrounded check for movement options
    + Resets jump/dash buttons so the player doesn't jump/dash by accident
    + Updated camera position
    + Switched to AddForce (Impulse) for jumps and dashes
    + Jumps now give a slight forward momentum to simulate pushback against the ground (affected by _hopSpeed)
    + Moved the appropriate movement options to their respective updates (FixedUpdate, LateUpdate)
    + Changed default gravity for Physics
    + Added comments to movement systems
    + Added mouse sensitivity variable to change camera panning speed
    
    - TODO: Adjust _isGrounded raycast to actual model radii
    - TODO: Figure out why sliding is still happening despite _Move() being called in LateUpdate(), after Physics happens in FixedUpdate()

--- Assets/Scripts/NPlayerInput.cs
+++ Assets/Scripts/NPlayerInput.cs
@@ -6,19 +6,25 @@ using UnityEngine.InputSystem;
 public class NPlayerInput : MonoBehaviour
 {
 
-    Vector2 _moveInput;
     Vector2 _mouseInput;
-    public float moveSpeed = 5.0f;
+    //NOTE: _mouseSpeed changes mouse sensitivity. Implement into options in the future
+    float _mouseSpeed = 1.2f;
+    Vector2 _moveInput;
+    public float moveSpeed = 10.0f;
     public float jumpSpeed = 5.0f;
-    public float dashSpeed = 20.0f;
+    public float hopSpeed = 0.25f;
+    public float dashSpeed = 2.5f;
+    bool _isGrounded = true;
+    RaycastHit terrain;
 
+    float _dashDuration = 0.0f;
     float _dashCooldown = 0.0f;
     float _isDashing = 0.0f;
     bool _dashed = false;
 
     bool _airDashed = false;
 
-    float _jumpCooldown = 0.0f;
+    float _jumpAnimDuration = 0.0f;
     float _isJumping = 0.0f;
     bool _jumped = false;
     bool _doubleJumped = false;
@@ -59,21 +65,9 @@ public class NPlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _MouseInput();
-        _Move();
-
-        _jumpCooldown -= Time.deltaTime;
-        _dashCooldown -= Time.deltaTime;
         _comboDuration -= Time.deltaTime;
         _animationDuration -= Time.deltaTime;
 
-        //Jump Movement
-        if (_isJumping == 1.0f)
-            _Jump();
-
-        //Dash Movement
-        if (_isDashing == 1.0f)
-            _Dash();
 
         if (insideCastingZone)
             _UseAbility();
@@ -82,6 +76,29 @@ public class NPlayerInput : MonoBehaviour
             _Attack();
     }
 
+    //Physics update (FixedUpdate); updates at set intervals
+    private void FixedUpdate() 
+    {
+        _jumpAnimDuration -= Time.fixedDeltaTime;
+        _dashDuration -= Time.fixedDeltaTime;
+        _dashCooldown -= Time.fixedDeltaTime;
+
+        //Jump Movement
+        if (_isJumping == 1.0f)
+            _Jump();
+
+        //Dash Movement
+        if (_isDashing == 1.0f)
+            _Dash();
+    }
+
+    //Called after physics (FixedUpdate); used to prevent sliding on slopes due to high gravity
+    private void LateUpdate() 
+    {
+        _MouseInput();
+        //TODO: Figure out why sliding happens regardless of y being updated in _Move() in LateUpdate()
+        _Move();
+    }
 
     void _UseAbility()
     {
@@ -108,7 +125,7 @@ public class NPlayerInput : MonoBehaviour
             rotationSpeedInverse = 1.0f;
 
         //rotate camera on x z plane
-        player.transform.rotation = Quaternion.AngleAxis(_mouseInput.x / rotationSpeedInverse, Vector3.up) * player.transform.rotation;
+        player.transform.rotation = Quaternion.AngleAxis(_mouseInput.x / rotationSpeedInverse * _mouseSpeed, Vector3.up) * player.transform.rotation;
 
         //increase/decrease height of camera target
         lookingAt.transform.position = lookingAt.transform.position + new Vector3(0.0f, _mouseInput.y / (rotationSpeedInverse * 10.0f), 0.0f);
@@ -126,60 +143,82 @@ public class NPlayerInput : MonoBehaviour
 
     void _Move()
     {
-        float y = player.GetComponent<Rigidbody>().velocity.y;
-        Vector3 vel = Quaternion.AngleAxis(270, Vector3.up) * ((Quaternion.AngleAxis(180, Vector3.up) * (transform.forward * _moveInput.x)) + (Quaternion.AngleAxis(90, Vector3.up) * (transform.forward * _moveInput.y))) * moveSpeed;
-        //NOTE: Dashing ignores camera direction when applied
-        if (_dashCooldown > 0.0f)
-            player.GetComponent<Rigidbody>().velocity = new Vector3(player.GetComponent<Rigidbody>().velocity.x, y, player.GetComponent<Rigidbody>().velocity.z);
-        else
+        if (_dashDuration < 0.0f)
+        {
+            //NOTE: Camera position affects the rotation of the player's movement, which is stored in the first value of Vector3 vel (Current: 135.0f)
+            Vector3 vel = Quaternion.AngleAxis(135.0f, Vector3.up) * ((Quaternion.AngleAxis(180, Vector3.up) * (transform.forward * _moveInput.x)) + (Quaternion.AngleAxis(90, Vector3.up) * (transform.forward * _moveInput.y)));
+            vel *= moveSpeed;
+
+            float y = player.GetComponent<Rigidbody>().velocity.y;
+            //NOTE: Checks for _isGrounded to reduce the effects of gravity such that the player doesn't slide off slopes
+            //TODO: Adjust raycast for actual models' radii
+            //NOTE: Raycasts downwards for terrain collision, checking at a distance of 0.6f (0.5f radius, 0.1f actual check)
+            _isGrounded = Physics.Raycast(transform.position, -transform.up, out terrain, 0.6f);
+            if (_isGrounded && terrain.transform.tag == "Terrain")
+            {
+                y = -1.0f;
+
+                //NOTE: Updates the state of dashes and jumps
+                _dashed = false;
+                _airDashed = false;
+                if (_jumpAnimDuration <= 0.0f)
+                {
+                    _jumped = false;
+                    _doubleJumped = false;
+                }
+            }
+
             player.GetComponent<Rigidbody>().velocity = new Vector3(vel.x, y, vel.z);
+        }
     }
 
     void _Jump()
     {
-        //TODO: Replace jump cooldown with a check for floor collision
-        if (_jumpCooldown < 0.0f)
+        //NOTE: Resets the button so that the player doesn't accidentally double jump
+         _isJumping = 0.0f;
+        //NOTE: Jumping adds a slight boost to the x/z direction you move in to simulate push back against the ground
+        //NOTE: Gravity is set to -24.525f. To change it: Edit -> Project Settings -> Physics -> y = newGravityValue
+        float jump = Mathf.Sqrt(jumpSpeed * -2.0f * -24.525f);
+        
+        if (_jumpAnimDuration <= 0.0f && _isGrounded && !_jumped)
         {
-            _jumped = false;
-            _doubleJumped = false;
-        }
-        if (_jumpCooldown < 1.8f && _jumped && !_doubleJumped)
-        {
-            player.GetComponent<Rigidbody>().velocity += new Vector3(0.0f, _isJumping, 0.0f) * jumpSpeed;
-            _doubleJumped = true;
-            _jumped = false;
-            _jumpCooldown = 2.5f;
-        }
-        if (_jumpCooldown < 0.0f && !_jumped)
-        {
-            player.GetComponent<Rigidbody>().velocity += new Vector3(0.0f, _isJumping, 0.0f) * jumpSpeed;
+            Vector3 vel = player.GetComponent<Rigidbody>().velocity;
+            player.GetComponent<Rigidbody>().AddForce(new Vector3(vel.x * hopSpeed, jump, vel.z * hopSpeed), ForceMode.Impulse);
             _jumped = true;
-            _jumpCooldown = 2.5f;
+            _jumpAnimDuration = 0.3f;
+        }
+        else if (_jumpAnimDuration <= 0.0f && !_doubleJumped)
+        {
+            Vector3 vel = player.GetComponent<Rigidbody>().velocity;
+            //NOTE: Adjusts double jump to give full jump height rather than being affected by gravity. Remove if necessary
+            player.GetComponent<Rigidbody>().velocity = new Vector3(vel.x, 0.0f, vel.z);
+            player.GetComponent<Rigidbody>().AddForce(new Vector3(vel.x * hopSpeed, jump, vel.z * hopSpeed), ForceMode.Impulse);
+            _doubleJumped = true;
+            _jumpAnimDuration = 0.3f;
         }
     }
 
     void _Dash()
     {
-        if (_dashed && !_airDashed && _jumped)
+        //NOTE: Resets the button so that the player doesn't accidentally dash + air dash while holding the button
+         _isDashing = 0.0f;
+
+        //NOTE: _dashCooldown stops dash chaining that would lead to acceleration
+        if (_dashCooldown <= 0.0f && _dashDuration <= 0.0f && !_dashed && _isGrounded)
         {
             Vector3 vel = player.GetComponent<Rigidbody>().velocity;
-            player.GetComponent<Rigidbody>().velocity = new Vector3(vel.x * dashSpeed, vel.y, vel.z * dashSpeed);
-            _airDashed = true;
-            //[TODO] Replace cooldown with a check on touching ground for air dashing
-            _dashCooldown = 1.5f;
-        }
-        if (_dashCooldown < 0.0f && !_dashed)
-        {
-            Vector3 vel = player.GetComponent<Rigidbody>().velocity;
-            player.GetComponent<Rigidbody>().velocity = new Vector3(vel.x * dashSpeed, vel.y, vel.z * dashSpeed);
+            player.GetComponent<Rigidbody>().AddForce(new Vector3(vel.x * dashSpeed, 0.0f, vel.z * dashSpeed), ForceMode.Impulse);
             _dashed = true;
-            //_dashCooldown represents duration of dash
-            _dashCooldown = 1.5f;
+            _dashDuration = 0.7f;
+            _dashCooldown = 1.0f;
         }
-        if (_dashCooldown < 0.0f)
+        else if (_dashCooldown <= 0.0f && _dashDuration <= 0.0f && !_airDashed)
         {
-            _dashed = false;
-            _airDashed = false;
+            Vector3 vel = player.GetComponent<Rigidbody>().velocity;
+            player.GetComponent<Rigidbody>().AddForce(new Vector3(vel.x * dashSpeed, 0.0f, vel.z * dashSpeed), ForceMode.Impulse);
+            _airDashed = true;
+            _dashDuration = 0.7f;
+            _dashCooldown = 1.0f;
         }
     }
 
