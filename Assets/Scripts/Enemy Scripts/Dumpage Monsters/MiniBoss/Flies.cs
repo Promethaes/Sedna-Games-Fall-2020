cb9f2c2455aaeb5db1242a793c72696b4d17ea9d
Anthony <anth19800@hotmail.com>
2020-11-24 16:20:16 -0500

    Update Flies.cs

--- Assets/Scripts/Enemy Scripts/Dumpage Monsters/MiniBoss/Flies.cs
+++ Assets/Scripts/Enemy Scripts/Dumpage Monsters/MiniBoss/Flies.cs
@@ -14,11 +14,15 @@ public class Flies : MonoBehaviour
     float _timer = 1.0f;
 
     public float damage = 10.0f;
+
+    public float lifetime = 3.0f;
+    float _lifetime = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
         _rigid = gameObject.GetComponent<Rigidbody>();
         _timer = maxTime;
+        _lifetime = lifetime;
     }
 
     // Update is called once per frame
@@ -34,6 +38,14 @@ public class Flies : MonoBehaviour
             target.GetComponent<PlayerBackend>().hp -= damage;
             _timer = maxTime;
         }
+        _lifetime -= Time.deltaTime;
+
+        if(_lifetime <= 0.0f)
+        {
+            _lifetime = lifetime;
+            gameObject.SetActive(false);
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
