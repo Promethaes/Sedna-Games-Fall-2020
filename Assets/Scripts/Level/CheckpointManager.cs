95664236e608779b58c345be99eae786b37589c8
Daniel Presas <daniel.presas@ontariotechu.net>
2020-11-24 17:12:00 -0500

    [Fix] Player downed bug

--- Assets/Scripts/Level/CheckpointManager.cs
+++ Assets/Scripts/Level/CheckpointManager.cs
@@ -2,44 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckpointManager : MonoBehaviour
-{
+public class CheckpointManager : MonoBehaviour {
     public Transform[] spawnPoints;
     public GamePlayerManager playerManager;
     public EnemySpawnPoint[] enemySpawners;
     int counter = 0;
 
-    private void Start() 
-    {
-        playerManager = FindObjectOfType<GamePlayerManager>();    
+    private void Start() {
+        playerManager = FindObjectOfType<GamePlayerManager>();
     }
 
-    public Vector3 getSpawn()
-    {
+    public Vector3 getSpawn() {
         return spawnPoints[counter].position;
     }
-    public void newSpawn()
-    {
+    public void newSpawn() {
         spawnPoints[counter].gameObject.SetActive(false);
         counter++;
     }
 
-    public void reset()
-    {
-        foreach (EnemySpawnPoint spawner in enemySpawners)
-        {
-            foreach (GameObject enemy in spawner.spawnEnemies)
-            {
+    public void reset() {
+        foreach(EnemySpawnPoint spawner in enemySpawners) {
+            foreach(GameObject enemy in spawner.spawnEnemies) {
                 enemy.SetActive(false);
                 enemy.GetComponent<EnemyData>().setHealth(enemy.GetComponent<EnemyData>().getMaxHealth());
             }
             spawner.setShouldSpawn(false);
         }
 
-        foreach (GameObject player in playerManager.players)
-        {
+        foreach(GameObject player in playerManager.players) {
             player.GetComponent<PlayerBackend>().hp = player.GetComponent<PlayerBackend>().maxHP;
             player.transform.position = spawnPoints[counter].position;
+            player.GetComponent<PlayerController>().downed = false;
         }
     }
 }
