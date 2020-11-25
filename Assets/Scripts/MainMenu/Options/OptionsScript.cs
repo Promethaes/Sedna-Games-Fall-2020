a42c4d1531c29ecac1f668ebc66f573cfa05d34f
Anthony <anth19800@hotmail.com>
2020-11-24 21:58:00 -0500

    [Wip] Main Menu

---
+++ Assets/Scripts/MainMenu/Options/OptionsScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsScript : MonoBehaviour
+{
+    public List<GameObject> options = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    bool toggle = false;
+    public void Toggle()
+    {
+        foreach (var option in options)
+        {
+            option.SetActive(!toggle);
+        }
+        toggle = !toggle;
+    }
+}
