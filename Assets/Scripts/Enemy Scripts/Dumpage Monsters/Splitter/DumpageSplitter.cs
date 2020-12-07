25f6ed647d789fc63e0e1bd31c9ca22907dc137c
Anthony <anth19800@hotmail.com>
2020-12-06 23:18:14 -0500

    [Update] Prefabs

--- Assets/Scripts/Enemy Scripts/Dumpage Monsters/Splitter/DumpageSplitter.cs
+++ Assets/Scripts/Enemy Scripts/Dumpage Monsters/Splitter/DumpageSplitter.cs
@@ -4,33 +4,30 @@ using UnityEngine;
 
 public class DumpageSplitter : MonoBehaviour
 {
-    public int poolSize = 2;
-    public GameObject splitling;
+    public List<GameObject> splitlings = new List<GameObject>();
     List<GameObject> _splitlings = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < poolSize;i++){
-            var temp = GameObject.Instantiate(splitling);
+        for (int i = 0; i < splitlings.Count; i++)
+        {
+            var temp = GameObject.Instantiate(splitlings[i]);
             temp.SetActive(false);
             temp.transform.position = gameObject.transform.position;
-            temp.transform.localScale = gameObject.transform.localScale * 0.5f;
+            temp.transform.localScale = gameObject.transform.localScale * 0.75f;
             temp.GetComponent<EnemyData>().randomScale = false;
-            temp.GetComponent<EnemyData>().enemyScale = gameObject.GetComponent<EnemyData>().enemyScale * 0.5f;
+            temp.GetComponent<EnemyData>().enemyScale = gameObject.GetComponent<EnemyData>().enemyScale * 0.75f;
             temp.GetComponent<EnemyData>().Init();
             _splitlings.Add(temp);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
-    }
-
-    private void OnDisable() {
-        for(int i = 0; i < _splitlings.Count;i++){
-            _splitlings[i].transform.position = gameObject.transform.position + new Vector3(i,1.0f,i);
+        for (int i = 0; i < _splitlings.Count; i++)
+        {
+            _splitlings[i].transform.position = gameObject.transform.position;
             _splitlings[i].SetActive(true);
         }
     }
