b30d94ca616c7adca6d8973a64c97df95137d60d
Anthony <anth19800@hotmail.com>
2021-02-16 16:51:47 -0500

    [Wip] Player Lobby to Test Game Scene

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -179,6 +179,8 @@ public class NetworkManager : MonoBehaviour
                 p.nMovement.readyPressed = false;
             }
             sceneChanger.changeScene(2);//TEMP
+            player.p.GetComponent<UnityEngine.InputSystem.PlayerInput>().SwitchCurrentActionMap("Gameplay");
+            DontDestroyOnLoad(player.p);
         }
 
     }
