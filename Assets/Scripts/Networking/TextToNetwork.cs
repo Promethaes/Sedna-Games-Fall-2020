54c4a03ae0e55e9869ff6e363de7c9d9a029ef2d
Daniel Presas <daniel.presas@ontariotechu.net>
2021-02-26 04:38:13 -0500

    [Merge] Remnants of the merge...

---
+++ Assets/Scripts/Networking/TextToNetwork.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class TextToNetwork : MonoBehaviour
+{
+    public TMP_InputField field;
+    public UDPClient client;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnSubmit(InputAction.CallbackContext ctx)
+    {
+        client.Send(field.text);
+        field.text = "";
+    }
+}
