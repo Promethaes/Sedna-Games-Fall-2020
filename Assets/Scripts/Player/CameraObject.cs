019d2f2c18c7ed820d9293dc0b45348f0a86b61a
Anthony <anth19800@hotmail.com>
2021-03-18 22:01:38 -0400

    [Fix] Camera

--- Assets/Scripts/Player/CameraObject.cs
+++ Assets/Scripts/Player/CameraObject.cs
@@ -4,31 +4,21 @@ using UnityEngine;
 
 public class CameraObject : MonoBehaviour
 {
-   static public SkinnedMeshRenderer player;
-   static public Cinemachine.CinemachineVirtualCamera myCamera;
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameObject temp = GameObject.FindGameObjectWithTag("virtualCam");
-        myCamera = temp.GetComponent<Cinemachine.CinemachineVirtualCamera>();
-    }
+    static public SkinnedMeshRenderer player;
+    static public Cinemachine.CinemachineVirtualCamera myCamera;
+    public GameObject cameraObject;
 
     private void Awake()
     {
-        GameObject temp = GameObject.FindGameObjectWithTag("virtualCam");
-        myCamera = temp.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        myCamera = cameraObject.GetComponent<Cinemachine.CinemachineVirtualCamera>();
     }
-
-    // Update is called once per frame
-    void Update()
+    static public void ChangeSkinnedMesh(SkinnedMeshRenderer newRenderer)
     {
-        //gameObject.transform.position = player.transform.position;
-        //gameObject.transform.rotation = player.transform.rotation;
-        //gameObject.transform.Rotate(new Vector3(0,1,0),180);
-        
-    }
-    static public  void ChangeSkinnedMesh(SkinnedMeshRenderer newRenderer)
-    {
+        if (!myCamera)
+        {
+            GameObject temp = GameObject.FindGameObjectWithTag("virtualCam");
+            myCamera = temp.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        }
         myCamera.m_LookAt = newRenderer.transform;
         myCamera.m_Follow = newRenderer.transform;
     }
