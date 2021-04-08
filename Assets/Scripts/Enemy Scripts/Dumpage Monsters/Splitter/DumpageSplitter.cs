7403155ec75d247b6e3bfd9dcf03580cacfe3436
Anthony <anth19800@hotmail.com>
2021-04-08 00:33:58 -0400

    [Wip] fix le bug

--- Assets/Scripts/Enemy Scripts/Dumpage Monsters/Splitter/DumpageSplitter.cs
+++ Assets/Scripts/Enemy Scripts/Dumpage Monsters/Splitter/DumpageSplitter.cs
@@ -27,7 +27,7 @@ public class DumpageSplitter : MonoBehaviour
     {
         for (int i = 0; i < _splitlings.Count; i++)
         {
-            if (!_splitlings[1])
+            if (!_splitlings[i])
                 continue;
             _splitlings[i].transform.position = gameObject.transform.position;
             _splitlings[i].SetActive(true);
