819b8778a40b40c631343a4eb4bbf5537a43ea29
Anthony <anth19800@hotmail.com>
2021-04-08 23:31:49 -0400

    [Add] Final Alpha Changes

---
+++ Assets/Scripts/EndGameScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameScript : MonoBehaviour
+{
+
+    IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(5.0f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Credits Scene");
+    }
+
+    bool started = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !started)
+        {
+            StartCoroutine("EndGame");
+            started = true;
+        }
+    }
+
+}
